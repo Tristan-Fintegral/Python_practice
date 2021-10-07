39291f64eb2d518d2ccf65bbee3fe745ec887c92
TristanMantle <tristan.mantle@fintgeral.com>
2021-10-07 15:03:46 +0100

    add more code regarding shocks

--- scenario_generator.py
+++ scenario_generator.py
@@ -1,18 +1,43 @@
 """
 Module to produce shocks for risk factors.
 """
-
 import numpy as np
+from matplotlib import pyplot
 
 
-def generate_log_normal_shocks(spot, vol, num_shocks=780):
+def generate_log_normal_shocks(vol, num_shocks=780):
     """Generate a vector of log normal shocks with given volatility.
+
+    Log shock = exp(vol * N(0,1))
+    S1 = S0 * (Log shock)
 
     :param float vol: Volatility in standard units
     :param int num_shocks: Number of shocks to produce
     :return [int]: Vector of shocks
     """
-    temp = 1
-    shock_vector = [vol]*num_shocks
+    rand_norm_vector = np.random.normal(loc=0, scale=1, size=num_shocks)
+    shock_vector = np.exp(vol * rand_norm_vector)
 
     return shock_vector
+
+
+
+def main():
+    shocks = generate_log_normal_shocks(vol=0.6, num_shocks=10000)
+    rel_shocks = shocks + 1
+
+    #pyplot.hist(shocks, bins=100)
+    #pyplot.hist(shocks, bins=100)
+    #pyplot.show()
+    prices=[None]*len(shocks)
+    prices[0]=1
+    for i in range(1,len(shocks)):
+        prices[i]=prices[i-1]*shocks[i-1]
+    #X = np.linspace(0, len(shocks),10000)
+
+    pyplot.plot(prices[0:500])
+    pyplot.show()
+
+
+if __name__ == '__main__':
+    main()
