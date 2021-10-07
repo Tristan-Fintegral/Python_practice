b7932e37f55ef9060ff48306c47e5a22efcd79db
Alex <alexanderdmottram@outlook.com>
2021-10-07 09:07:38 +0100

    Added function signature for generating log normal shocks. Implementation has not been completed.

---
+++ scenario_generator.py
@@ -0,0 +1,18 @@
+"""
+Module to produce shocks for risk factors.
+"""
+
+import numpy as np
+
+
+def generate_log_normal_shocks(spot, vol, num_shocks=780):
+    """Generate a vector of log normal shocks with given volatility.
+
+    :param float vol: Volatility in standard units
+    :param int num_shocks: Number of shocks to produce
+    :return [int]: Vector of shocks
+    """
+
+    shock_vector = [vol]*num_shocks
+
+    return shock_vector
