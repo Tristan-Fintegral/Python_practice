9582e84562312036b1323046c2e3013d3b39075b
Alex <alexanderdmottram@outlook.com>
2021-10-11 08:44:42 +0100

    Move example py files into separate folder.

---
+++ examples/test.py
@@ -0,0 +1,14 @@
+import scenario_generator
+
+
+for i in range(10):
+    print(i)
+
+def main():
+    print('I am test file')
+
+print('Main in test has been defined.')
+
+if __name__ == '__main__':
+    main()
+
