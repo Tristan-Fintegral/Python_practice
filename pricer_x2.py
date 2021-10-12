4f1fb6fed0f145d00638b3665e2081c8793ee151
Alex <alexanderdmottram@outlook.com>
2021-10-12 09:23:18 +0100

    Added arg and kwarg example and option to choose pricing method.

--- pricer_x2.py
+++ pricer_x2.py
@@ -3,27 +3,41 @@ import scenario_generator
 from matplotlib import pyplot
 
 
+class OptionPricerType:
+    ANALYTICAL = 'Analytical'
+    MONTE_CARLO = 'Monte Carlo'
+    A_THIRD_PRICER = 'Pricer Three'
+    ALL_PRICERS = [ANALYTICAL, MONTE_CARLO, A_THIRD_PRICER]
+
+
 def create_bsm_process(spot, vol, rfr, div):
     """
-    This function creates BSM pricer given market values
+    This function creates BSM process given market values
 
     :param float div: flat forward dividend rate using actual/365 fixed
     :param int spot: spot price
     :param float vol: volatility
     :param float rfr: flat forward risk free rate using actual/365 fixed
-
+    :return BlackScholesMertonProcess: Market data process
     """
     initialValue = ql.QuoteHandle(ql.SimpleQuote(spot))
-    sigma = vol
     today = ql.Date().todaysDate()
-    riskFreeTS = ql.YieldTermStructureHandle(ql.FlatForward(today, rfr, ql.Actual365Fixed()))
-    dividendTS = ql.YieldTermStructureHandle(ql.FlatForward(today, div, ql.Actual365Fixed()))
-    volTS = ql.BlackVolTermStructureHandle(ql.BlackConstantVol(today, ql.NullCalendar(), sigma, ql.Actual365Fixed()))
-    bsm_process = ql.BlackScholesMertonProcess(initialValue, dividendTS, riskFreeTS, volTS)
+    riskFreeTS = ql.YieldTermStructureHandle(
+        ql.FlatForward(today, rfr, ql.Actual365Fixed())
+    )
+    dividendTS = ql.YieldTermStructureHandle(
+        ql.FlatForward(today, div, ql.Actual365Fixed())
+    )
+    volTS = ql.BlackVolTermStructureHandle(
+        ql.BlackConstantVol(today, ql.NullCalendar(), vol, ql.Actual365Fixed())
+    )
+    bsm_process = ql.BlackScholesMertonProcess(
+        initialValue, dividendTS, riskFreeTS, volTS
+    )
     return bsm_process
 
 
-def create_call_option(strike, maturity_date, process):
+def create_call_option(strike, maturity_date, process, pricer_type=None):
     """
        This function creates call option using BSM pricer
        created by create_bsm_process with strike price and maturity date. This
@@ -31,9 +45,21 @@ def create_call_option(strike, maturity_date, process):
 
        :param int strike: strike price of option
        :param datetime maturity_date: maturity date of option
-
        """
-    engine = ql.AnalyticEuropeanEngine(process)
+    pricer_type = pricer_type or OptionPricerType.ANALYTICAL
+    if pricer_type not in OptionPricerType.ALL_PRICERS:
+        raise RuntimeError(f'PUT STATEMENT HERE')
+
+    if pricer_type == OptionPricerType.ANALYTICAL:
+        engine = ql.AnalyticEuropeanEngine(process)
+    elif pricer_type == OptionPricerType.MONTE_CARLO:
+        rng = "pseudorandom"  # could use "lowdiscrepancy"
+        engine = ql.MCEuropeanEngine(
+            process, rng, timeSteps=2, requiredSamples=10000
+        )
+    else:
+        raise RuntimeError(f'PUT STATEMENT HERE')
+
     option_type = ql.Option.Call
     payoff = ql.PlainVanillaPayoff(option_type, strike)
     europeanExercise = ql.EuropeanExercise(maturity_date)
