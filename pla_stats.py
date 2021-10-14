c4a6011ac6a42426405728783bf3bedcddc417fb
AnishManjiani <anish0210@gmail.com>
2021-10-14 10:16:43 +0100

    added pla_stats method to calculate Spearman Correlation and Kolmogorov-Smirnov metrics.

--- pla_stats.py
+++ pla_stats.py
@@ -1,10 +1,36 @@
 # TODO FOCUS -> Logging, clean code, doc strings, well thought out functions
+import logging
+from time import asctime
+
+from scipy.stats import ks_2samp, spearmanr
+
+logger = logging.getLogger(__name__)
+
 
 def pla_stats(fo_pnl, risk_pnl):
-    # TODO -> Anish
-    return {
-        'ks_val': None,
-        'ks_p': None,
-        'spearman_corr': None,
-        'spearman_p':None
-    }
+    """
+    :param fo_pnl: HTPL is produced by revaluing the positions held at the end of the previous day using the market data
+    at the end of the current day.
+    :param risk_pnl: RTPL is the daily trading desk-level P&L produced by the valuation engine of the trading desk’s
+    risk management model.
+    :return Spearman and ks values
+
+    kolmogorov-smirnov(ks): test metric to assess the similarity of the distributions of RTPL and HPL.
+    Spearman Correlation: metric to assess correlation between RTPL and HPL.
+
+    """
+
+    ks_values = ks_2samp(fo_pnl, risk_pnl)[0]
+
+    spear_values = spearmanr(fo_pnl, risk_pnl)[0]
+
+    logger.info(
+        f"{asctime} Retrieving pla statistics, "
+        f"Spearman Correlation: {spear_values} and dividend rate: {ks_values}."
+    )
+
+    return spear_values, ks_values
+
+
+#if __name__ == '__main__':
+#    pla_stats(fo_pnl, risk_pnl)
