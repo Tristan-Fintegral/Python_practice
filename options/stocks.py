66f3e8508abeed9fa3e9bce3008c199773bf0378
nikontogi <nikolas.kontogiannis@fintegral.com>
2021-10-26 13:11:27 +0100

    created class for stocks

---
+++ options/stocks.py
@@ -0,0 +1,22 @@
+"""
+Create a class for storing stock holdings
+"""
+
+
+class Stock:
+    def __init__(self, stock_name, num_shares):
+        self.stock_name = stock_name
+        self.num_shares = num_shares
+
+    def price(self, spot=100):
+        return self.num_shares * spot
+
+
+def stock_example():
+    stock_name = 'aapl'
+    num_shares = 50
+    aapl = Stock(stock_name,num_shares)
+    print(f"I have {aapl.price()} of {stock_name} stock")
+
+if __name__ == '__main__':
+    stock_example()
